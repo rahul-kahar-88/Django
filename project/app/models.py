d253de8cb68064ab9d60bd5147d86a6f7fc88d10
rahul kahar 88 <rahulkahar88588@gmail.com>
2026-04-18 15:44:23 +0530

    update

--- project/app/models.py
+++ project/app/models.py
@@ -37,7 +37,7 @@ class Query(models.Model):
     Emp_id=models.CharField(max_length=40)
     Dept=models.CharField(max_length=40)
     Query=models.CharField(max_length=80)
-    Status=models.CharField(default="pending")
+    Status=models.CharField(default="pending" , max_length=20)
     Reply=models.CharField(max_length=80,null=True)
     
 
